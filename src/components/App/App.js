a3b03112485d4e7f43e509d5229fd3f55b7d4639
asosunoff <al.sosunoff@gmail.com>
2020-10-13 00:14:20 +0300

    Added Calendar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Calendar from "../RangePicker/Calendar";
 
 const App = () => {
-	return <div></div>;
-}
+	return <Calendar />;
+};
 
 export default App;
---
+++ src/components/RangePicker/Calendar/Calendar.js
@@ -0,0 +1,37 @@
+import React, { useMemo } from "react";
+import Days from "../Days";
+import styles from "./Calendar.module.css";
+
+const Calendar = ({
+	date = new Date(),
+	from,
+	to,
+	onSetDay = () => {},
+} = {}) => {
+	const localeString = useMemo(
+		() => date.toLocaleString("ru", { month: "long" }),
+		[date]
+	);
+
+	return (
+		<div className={styles.rangepicker__calendar}>
+			<div className={styles["rangepicker__month-indicator"]}>
+				<time dateTime={localeString}>{localeString}</time>
+			</div>
+
+			<div className={styles["rangepicker__day-of-week"]}>
+				<div>Пн</div>
+				<div>Вт</div>
+				<div>Ср</div>
+				<div>Чт</div>
+				<div>Пт</div>
+				<div>Сб</div>
+				<div>Вс</div>
+			</div>
+
+			<Days date={date} from={from} to={to} onSetDay={onSetDay} />
+		</div>
+	);
+};
+
+export default Calendar;

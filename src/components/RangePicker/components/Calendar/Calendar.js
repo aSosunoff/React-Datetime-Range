a703379acee3e14809dc4b377eee40cdc0f5d1c3
asosunoff <al.sosunoff@gmail.com>
2020-10-16 23:44:12 +0300

    refactor

--- src/components/RangePicker/components/Calendar/Calendar.js
+++ src/components/RangePicker/components/Calendar/Calendar.js
@@ -4,42 +4,40 @@ import Day from "../Day";
 import styles from "./Calendar.module.scss";
 
 const Calendar = ({ date, from, to } = {}) => {
-	const localeString = useMemo(
-		() => date.toLocaleString("ru", { month: "long" }),
-		[date]
-	);
+  const localeString = useMemo(
+    () => date.toLocaleString("ru", { month: "long" }),
+    [date]
+  );
 
-	const { days } = useDays(date, from, to);
+  const { days } = useDays(date, from, to);
 
-	return (
-		<div className={styles.rangepicker__calendar}>
-			<div className={styles["rangepicker__month-indicator"]}>
-				<time dateTime={localeString}>{localeString}</time>
-			</div>
+  const dayOfWeekName = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
 
-			<div className={styles["rangepicker__day-of-week"]}>
-				<div>Пн</div>
-				<div>Вт</div>
-				<div>Ср</div>
-				<div>Чт</div>
-				<div>Пт</div>
-				<div>Сб</div>
-				<div>Вс</div>
-			</div>
+  return (
+    <div className={styles.rangepicker__calendar}>
+      <div className={styles["rangepicker__month-indicator"]}>
+        <time dateTime={localeString}>{localeString}</time>
+      </div>
 
-			<div className={styles["rangepicker__date-grid"]}>
-				{days.map(({ dayNumber, gridColumnStart, type, date }) => (
-					<Day
-						key={dayNumber}
-						number={dayNumber}
-						gridColumnStart={gridColumnStart}
-						type={type}
-						date={date}
-					/>
-				))}
-			</div>
-		</div>
-	);
+      <div className={styles["rangepicker__day-of-week"]}>
+        {dayOfWeekName.map((name) => (
+          <div>{name}</div>
+        ))}
+      </div>
+
+      <div className={styles["rangepicker__date-grid"]}>
+        {days.map(({ dayNumber, gridColumnStart, type, date }) => (
+          <Day
+            key={dayNumber}
+            number={dayNumber}
+            gridColumnStart={gridColumnStart}
+            type={type}
+            date={date}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Calendar;
--- src/components/RangePicker/contexts/day.js
+++ src/components/RangePicker/contexts/day.js
@@ -3,8 +3,6 @@ import React, { createContext } from "react";
 export const DayContext = createContext();
 DayContext.displayName = "DayContext";
 
-export const DayConsumer = DayContext.Consumer;
-
 export const DayProvider = ({ setDayHandler, children }) => {
 	return (
 		<DayContext.Provider value={{ setDay: (date) => setDayHandler(date) }}>

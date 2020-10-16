6a05e1e21ec13bc6c3a9d385dc91785da12c4959
asosunoff <al.sosunoff@gmail.com>
2020-10-16 20:26:09 +0300

    App fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,50 +1,48 @@
-import React, { useMemo, useRef, useState } from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
 import Button from "../Button/Button";
 import RangePicker from "../RangePicker";
 
 const format = (date) =>
-	date.toLocaleString("ru", {
-		year: "numeric",
-		month: "long",
-		day: "numeric",
-	});
+  date.toLocaleString("ru", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
 const App = () => {
-	const targetButton = useRef(null);
+  const targetButton = useRef(null);
 
-	const [isOpen, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
 
-	const [range, setRange] = useState({ from: null, to: null });
+  const [range, setRange] = useState({ from: null, to: null });
 
-	const displayRange = useMemo(
-		() =>
-			range.from && range.to
-				? `${format(range.from)} - ${format(range.to)}`
-				: null,
-		[range.from, range.to]
-	);
+  const displayRange = useMemo(
+    () =>
+      range.from && range.to
+        ? `${format(range.from)} - ${format(range.to)}`
+        : null,
+    [range.from, range.to]
+  );
 
-	const setRangeHandler = ({ from, to }) => setRange({ from, to });
+  const toggleRangePicker = useCallback(() => setOpen(!isOpen), [isOpen]);
 
-	const toggleRangePicker = () => setOpen(!isOpen);
+  const closeRangePicker = useCallback(() => setOpen(false), []);
 
-	const closeRangePicker = () => setOpen(false);
+  return (
+    <>
+      <Button ref={targetButton} onClick={toggleRangePicker}>
+        {displayRange || "Открыть"}
+      </Button>
 
-	return (
-		<>
-			<Button ref={targetButton} onClick={toggleRangePicker}>
-				{displayRange || "Открыть"}
-			</Button>
-
-			<RangePicker
-				isOpen={isOpen}
-				{...range}
-				onRangeSelected={setRangeHandler}
-				target={targetButton.current}
-				onClose={closeRangePicker}
-			/>
-		</>
-	);
+      <RangePicker
+        isOpen={isOpen}
+        {...range}
+        onRangeSelected={setRange}
+        target={targetButton.current}
+        onClose={closeRangePicker}
+      />
+    </>
+  );
 };
 
 export default App;

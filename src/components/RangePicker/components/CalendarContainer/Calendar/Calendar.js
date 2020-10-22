813e71b4294dcb8a86b22f2cc93a6d10bc854b38
asosunoff <al.sosunoff@gmail.com>
2020-10-22 14:17:48 +0300

    Added Day test

--- src/components/RangePicker/components/CalendarContainer/Calendar/Calendar.js
+++ src/components/RangePicker/components/CalendarContainer/Calendar/Calendar.js
@@ -1,10 +1,11 @@
 import React, { useMemo } from "react";
 import useDays from "../../../hooks/useDays";
+import PropTypes from "prop-types";
 import Day from "./Day";
 import WeekLine from "./WeekLine";
 import styles from "./Calendar.module.scss";
 
-const Calendar = ({ date, from, to, locales } = {}) => {
+const Calendar = ({ date, from, to, locales }) => {
   const localeString = useMemo(
     () => date.toLocaleString(locales, { month: "long", year: "numeric" }),
     [date, locales]
@@ -14,13 +15,13 @@ const Calendar = ({ date, from, to, locales } = {}) => {
 
   return (
     <div className={styles.calendar}>
-      <div className={styles.title}>
+      <div className={styles.title} data-id="calendar-title">
         <time dateTime={localeString}>{localeString}</time>
       </div>
 
       <WeekLine locales={locales} />
 
-      <div className={styles.grid}>
+      <div className={styles.grid} data-id="calendar-container">
         {days.map(
           ({
             dayNumber,
@@ -46,6 +47,20 @@ const Calendar = ({ date, from, to, locales } = {}) => {
       </div>
     </div>
   );
+};
+
+Calendar.defaultProps = {
+  /* date,
+  from,
+  to, */
+  locales: "ru",
+};
+
+Calendar.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+  from: PropTypes.instanceOf(Date),
+  to: PropTypes.instanceOf(Date),
+  locales: PropTypes.string,
 };
 
 export default Calendar;
---
+++ src/components/RangePicker/components/CalendarContainer/Calendar/Calendar.test.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import Calendar from "./Calendar";
+import { DayProvider } from "../../../contexts/day";
+
+describe("WeekLine", () => {
+  let component;
+
+  const getByDataId = (dataId) => component.find(`[data-id="${dataId}"]`);
+  const getProp = (prop) => component.prop(prop);
+  const setProp = (prop, value) => component.setProps({ [prop]: value });
+
+  const title = () => getByDataId("calendar-title");
+  const container = () => getByDataId("calendar-container");
+
+  const props = {
+    date: new Date(2020, 0, 1),
+  };
+
+  beforeEach(() => {
+    component = mount(<Calendar {...props} />, {
+      wrappingComponent: DayProvider,
+    });
+  });
+
+  it("should render", () => {
+    expect(component).toHaveLength(1);
+  });
+
+  it("should contain title (default)", () => {
+    const localesDefault = getProp("locales");
+    let localeString = props.date.toLocaleString(localesDefault, {
+      month: "long",
+      year: "numeric",
+    });
+    expect(title().find("time").text()).toBe(localeString);
+  });
+
+  it("should contain title (en)", () => {
+    setProp("locales", "en");
+    const localeString = props.date.toLocaleString("en", {
+      month: "long",
+      year: "numeric",
+    });
+    expect(title().find("time").text()).toBe(localeString);
+  });
+
+  it("should contain length 31 day", () => {
+    expect(container().children()).toHaveLength(31);
+  });
+
+  it("should contain length 29 day", () => {
+    setProp("date", new Date(2020, 1, 1));
+    expect(container().children()).toHaveLength(29);
+  });
+});

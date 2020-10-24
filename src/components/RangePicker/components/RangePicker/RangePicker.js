593b1fdc60378412517fae51dc62c0cfe573ce4b
asosunoff <al.sosunoff@gmail.com>
2020-10-24 14:39:57 +0300

    Range picke fix

--- src/components/RangePicker/components/RangePicker/RangePicker.js
+++ src/components/RangePicker/components/RangePicker/RangePicker.js
@@ -1,17 +1,19 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useContext, useEffect, useMemo } from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
 
 import styles from "./RangePicker.module.scss";
 import variable from "./variable.module.scss";
-import { DayProvider } from "../../contexts/day";
 import useRange from "../../hooks/useRange";
 import BottomBar from "./../BottomBar/BottomBar";
 import TimePicker from "./../TimePicker/TimePicker";
 import CalendarContainer from "./../CalendarContainer";
-import withControl from "../../HOC/withControl";
-import withAnimation from "../../HOC/withAnimation";
-import withTimePickerContext from "../../HOC/withTimePickerContext";
+import { withControl } from "../../HOC/withControl";
+import { withAnimation } from "../../HOC/withAnimation";
+import { withTimePickerContext } from "../../HOC/withTimePickerContext";
+import { withDayContext } from "../../HOC/withDayContext";
+import { DayContext } from "../../contexts/day";
+import { compose } from "../../utils/compose";
 
 const format = (locales, date) =>
   date.toLocaleString(locales, {
@@ -26,7 +28,6 @@ const format = (locales, date) =>
 const RangePicker = React.forwardRef(
   (
     {
-      month,
       startDate,
       endDate,
       onClose,
@@ -34,6 +35,7 @@ const RangePicker = React.forwardRef(
       calendarVisibleCount,
       locales,
       style,
+      month,
       Control,
     },
     ref
@@ -65,56 +67,54 @@ const RangePicker = React.forwardRef(
       [_endDate, _startDate, locales]
     );
 
-    const setDayHandler = useCallback(
-      (date) => {
-        let from = null;
-        let to = null;
-        if (!_startDate || _endDate) {
-          from = date;
-        } else {
-          const current = date.getTime();
-          to = new Date(Math.max(current, _startDate.getTime()));
-          from = new Date(Math.min(current, _startDate.getTime()));
-        }
-        setRangeHandler(from, to);
-      },
-      [_startDate, _endDate, setRangeHandler]
-    );
+    const { day } = useContext(DayContext);
+
+    useEffect(() => {
+      let from = null;
+      let to = null;
+      if (!_startDate || _endDate) {
+        from = day;
+      } else {
+        const current = day.getTime();
+        to = new Date(Math.max(current, _startDate.getTime()));
+        from = new Date(Math.min(current, _startDate.getTime()));
+      }
+      setRangeHandler(from, to);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [day]);
 
     return (
-      <DayProvider setDayHandler={setDayHandler}>
-        <div
-          ref={ref}
-          className={cn(styles.rangepicker, variable["range-picker-variable"])}
-          style={style}
-        >
-          <Control />
+      <div
+        ref={ref}
+        className={cn(styles.rangepicker, variable["range-picker-variable"])}
+        style={style}
+      >
+        <Control />
 
-          <CalendarContainer
-            calendarVisibleCount={calendarVisibleCount}
-            startDate={_startDate}
-            endDate={_endDate}
-            locales={locales}
-            showMonth={month}
-          />
+        <CalendarContainer
+          calendarVisibleCount={calendarVisibleCount}
+          startDate={_startDate}
+          endDate={_endDate}
+          locales={locales}
+          showMonth={month}
+        />
 
-          <TimePicker
-            startDate={_startDate}
-            endDate={_endDate}
-            onSetTimeStart={setTimeFromHandler}
-            onSetTimeEnd={setTimeToHandler}
-          />
+        <TimePicker
+          startDate={_startDate}
+          endDate={_endDate}
+          onSetTimeStart={setTimeFromHandler}
+          onSetTimeEnd={setTimeToHandler}
+        />
 
-          <BottomBar title={rangeString}>
-            <div onClick={applyHandler} data-id="apply-button">
-              применить
-            </div>
-            <div onClick={resetHandler} data-id="clear-button">
-              очистить
-            </div>
-          </BottomBar>
-        </div>
-      </DayProvider>
+        <BottomBar title={rangeString}>
+          <div onClick={applyHandler} data-id="apply-button">
+            применить
+          </div>
+          <div onClick={resetHandler} data-id="clear-button">
+            очистить
+          </div>
+        </BottomBar>
+      </div>
     );
   }
 );
@@ -126,14 +126,32 @@ RangePicker.defaultProps = {
   locales: "ru",
 };
 
-RangePicker.propTypes = {
+const defaultProps = {
   startDate: PropTypes.instanceOf(Date),
   endDate: PropTypes.instanceOf(Date),
   onClose: PropTypes.func,
   onRangeSelected: PropTypes.func,
   calendarVisibleCount: PropTypes.number.isRequired,
   locales: PropTypes.string,
+};
+
+const animationProps = {
   style: PropTypes.object,
 };
 
-export default withTimePickerContext(withAnimation(withControl(RangePicker)));
+const controlProps = {
+  month: PropTypes.instanceOf(Date),
+};
+
+RangePicker.propTypes = {
+  ...defaultProps,
+  ...animationProps,
+  ...controlProps,
+};
+
+export default compose(
+  withDayContext,
+  withTimePickerContext,
+  withAnimation,
+  withControl
+)(RangePicker);

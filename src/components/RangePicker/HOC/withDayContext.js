c5a2e8401bbb6284188bc4f8e812d03b5430a5d2
asosunoff <al.sosunoff@gmail.com>
2020-10-26 23:06:17 +0300

    with Day Context added display name

--- src/components/RangePicker/HOC/withDayContext.js
+++ src/components/RangePicker/HOC/withDayContext.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { DayProvider } from "../contexts/day";
 
-export const withDayContext = (WrapperComponent) => (props) => {
-  return (
-    <DayProvider>
-      <WrapperComponent {...props} />
-    </DayProvider>
-  );
+export const withDayContext = (WrapperComponent) => {
+  const WithDayContext = (props) => {
+    return (
+      <DayProvider>
+        <WrapperComponent {...props} />
+      </DayProvider>
+    );
+  };
+
+  return WithDayContext;
 };

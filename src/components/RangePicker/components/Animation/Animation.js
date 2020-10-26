88640bccd19baec35d214986d0678218d62e7b91
asosunoff <al.sosunoff@gmail.com>
2020-10-26 11:39:53 +0300

    Animation fix

--- src/components/RangePicker/components/Animation/Animation.js
+++ src/components/RangePicker/components/Animation/Animation.js
@@ -1,47 +1,58 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import PropTypes from "prop-types";
 import { Transition } from "react-transition-group";
 
 const Animation = ({ inProp, top, left, duration, children }) => {
-  const defaultStyle = {
-    transition: `
+  const defaultStyle = useMemo(
+    () => ({
+      transition: `
 			opacity ${duration}ms ease-in-out,
 			transform ${duration}ms ease-in-out,
 			box-shadow .2s ease-out`,
-    opacity: 0,
-    transform: "translateY(50px)",
-    left: "-10000px",
-    top: "-10000px",
-  };
+      opacity: 0,
+      transform: "translateY(50px)",
+      left: "-10000px",
+      top: "-10000px",
+    }),
+    [duration]
+  );
 
-  const transitionStyles = {
-    entering: {
-      opacity: 1,
-      transform: "translateY(0px)",
-      left: `${left}px`,
-      top: `${top}px`,
+  const transitionStyles = useMemo(
+    () => ({
+      entering: {
+        opacity: 1,
+        transform: "translateY(0px)",
+        left: `${left}px`,
+        top: `${top}px`,
+      },
+      entered: {
+        opacity: 1,
+        transform: "translateY(0px)",
+        left: `${left}px`,
+        top: `${top}px`,
+      },
+      exiting: {
+        left: `${left}px`,
+        top: `${top}px`,
+      },
+      exited: {},
+    }),
+    [left, top]
+  );
+
+  const renderProps = useCallback(
+    (state) => {
+      return children({
+        ...defaultStyle,
+        ...transitionStyles[state],
+      });
     },
-    entered: {
-      opacity: 1,
-      transform: "translateY(0px)",
-      left: `${left}px`,
-      top: `${top}px`,
-    },
-    exiting: {
-      left: `${left}px`,
-      top: `${top}px`,
-    },
-    exited: {},
-  };
+    [children, defaultStyle, transitionStyles]
+  );
 
   return (
     <Transition in={inProp} timeout={duration}>
-      {(state) =>
-        children({
-          ...defaultStyle,
-          ...transitionStyles[state],
-        })
-      }
+      {renderProps}
     </Transition>
   );
 };

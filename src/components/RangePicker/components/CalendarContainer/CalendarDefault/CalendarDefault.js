312475a1f57110cd21cb801c056b43744bb2f8d7
asosunoff <al.sosunoff@gmail.com>
2020-10-27 20:31:08 +0300

    CalendarDefault fix

--- src/components/RangePicker/components/CalendarContainer/CalendarDefault/CalendarDefault.js
+++ src/components/RangePicker/components/CalendarContainer/CalendarDefault/CalendarDefault.js
@@ -1,23 +1,32 @@
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
 import useCalendar from "../../../hooks/useCalendar";
 import PropTypes from "prop-types";
 import Day from "../Day";
 import WeekLine from "../WeekLine";
 import styles from "./CalendarDefault.module.scss";
 
-const CalendarDefault = ({ date, from, to, locales }) => {
+const CalendarDefault = ({ date, from, to, locales, TitleComponentRender }) => {
+  const { days } = useCalendar(date, from, to);
+
   const localeString = useMemo(
     () => date.toLocaleString(locales, { month: "long", year: "numeric" }),
     [date, locales]
   );
 
-  const { days } = useCalendar(date, from, to);
+  const TitleComponent = useCallback(
+    ({ titleClass }) => (
+      <div className={titleClass} data-test-id="calendar-title">
+        <time dateTime={localeString}>{localeString}</time>
+      </div>
+    ),
+    [localeString]
+  );
+
+  let Title = TitleComponentRender || TitleComponent;
 
   return (
     <div className={styles.calendar}>
-      <div className={styles.title} data-test-id="calendar-title">
-        <time dateTime={localeString}>{localeString}</time>
-      </div>
+      <Title titleClass={styles.title} />
 
       <WeekLine locales={locales} />
 
@@ -39,6 +48,7 @@ CalendarDefault.propTypes = {
   from: PropTypes.instanceOf(Date),
   to: PropTypes.instanceOf(Date),
   locales: PropTypes.string,
+  /* TitleComponent: PropTypes.element, */
 };
 
 export default CalendarDefault;

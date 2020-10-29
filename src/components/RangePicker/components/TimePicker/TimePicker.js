0821f82b39ff890a2a95c3621c10d7b86f9a8c8e
asosunoff <al.sosunoff@gmail.com>
2020-10-30 01:11:52 +0300

    Timepicker component fix

--- src/components/RangePicker/components/TimePicker/TimePicker.js
+++ src/components/RangePicker/components/TimePicker/TimePicker.js
@@ -1,58 +1,46 @@
-import React, { useMemo } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styles from "./TimePicker.module.scss";
 
-const TimePicker = ({ startDate, endDate, onSetTimeStart, onSetTimeEnd }) => {
-  const timeFrom = useMemo(
-    () => (startDate ? startDate.toLocaleTimeString() : ""),
-    [startDate]
-  );
-
-  const timeTo = useMemo(() => (endDate ? endDate.toLocaleTimeString() : ""), [
-    endDate,
-  ]);
-
-  const getFormatTime = (value) => {
-    const [hour = 0, minute = 0, second = 0] = value.split(":").map(Number);
-
-    return [hour, minute, second];
-  };
-
+const TimePicker = ({
+  startTime,
+  endTime,
+  isDisabledStartTime,
+  isDisabledEndTime,
+  onSetTimeStart,
+  onSetTimeEnd,
+}) => {
   return (
     <div className={styles.time_picker} data-test-id="time-picker">
       <input
         data-test-id="time-picker-start"
         type="time"
         step="1"
-        disabled={!Boolean(timeFrom)}
-        onChange={({ target }) =>
-          onSetTimeStart(...getFormatTime(target.value))
-        }
-        value={timeFrom}
+        disabled={isDisabledStartTime}
+        onChange={({ target }) => onSetTimeStart(target.value)}
+        value={startTime}
       />
 
       <input
         data-test-id="time-picker-end"
         type="time"
         step="1"
-        disabled={!Boolean(timeTo)}
-        onChange={({ target }) => onSetTimeEnd(...getFormatTime(target.value))}
-        value={timeTo}
+        disabled={isDisabledEndTime}
+        onChange={({ target }) => onSetTimeEnd(target.value)}
+        value={endTime}
       />
     </div>
   );
 };
 
 TimePicker.defaultProps = {
-  startDate: null,
-  endDate: null,
   onSetTimeStart: () => {},
   onSetTimeEnd: () => {},
 };
 
 TimePicker.propTypes = {
-  startDate: PropTypes.instanceOf(Date),
-  endDate: PropTypes.instanceOf(Date),
+  startTime: PropTypes.string, // TODO: ВОТКНУТЬ ПРОВЕРКУ
+  endTime: PropTypes.string, // TODO: ВОТКНУТЬ ПРОВЕРКУ
   onSetTimeStart: PropTypes.func,
   onSetTimeEnd: PropTypes.func,
 };

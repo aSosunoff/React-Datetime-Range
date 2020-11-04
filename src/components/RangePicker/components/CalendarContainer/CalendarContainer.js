bd08d69e8a426ebad7c95c924e91338c0fe8e022
asosunoff <al.sosunoff@gmail.com>
2020-11-04 22:30:42 +0300

    Fix data-test-id

--- src/components/RangePicker/components/CalendarContainer/CalendarContainer.js
+++ src/components/RangePicker/components/CalendarContainer/CalendarContainer.js
@@ -29,6 +29,7 @@ const CalendarContainer = ({ locales }) => {
       style={{
         gridTemplateColumns: `repeat(${calendars.length}, 1fr)`,
       }}
+      data-test-id="calendar-container"
     >
       {calendars.map(({ date, days }, index) =>
         index === 0 ? (
--- src/components/RangePicker/components/CalendarContainer/CalendarDefault/CalendarDefault.js
+++ src/components/RangePicker/components/CalendarContainer/CalendarDefault/CalendarDefault.js
@@ -10,7 +10,7 @@ const CalendarDefault = ({ days, locales, children: Title }) => (
 
     <WeekLine locales={locales} />
 
-    <div className={styles.grid} data-test-id="calendar-container">
+    <div className={styles.grid} data-test-id="calendar-container-day">
       {days.map((day) => (
         <Day key={day.index} {...day} />
       ))}

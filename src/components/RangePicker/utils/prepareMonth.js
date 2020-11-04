790a1207f757b169107565e3448fd756948bd12e
asosunoff <al.sosunoff@gmail.com>
2020-11-04 14:02:08 +0300

    fix prepareMonth. unnecessary render day

--- src/components/RangePicker/utils/prepareMonth.js
+++ src/components/RangePicker/utils/prepareMonth.js
@@ -8,16 +8,13 @@ import {
 export const prepareMonth = (date) => {
   const { year, month } = getDateSplit(date);
 
-  const getDateTimestamp = (number) => new Date(year, month, number).getTime();
-
   return {
     days: new Array(getMonthDayCount(date))
       .fill(null)
       .map((_, index) => index + 1)
       .map((number) => ({
         number,
-        date: new Date(getDateTimestamp(number)),
-        dateTimestamp: getDateTimestamp(number),
+        dateTimestamp: new Date(year, month, number).getTime(),
       })),
     firsDayOfWeekByMonth: getNumberFirsDayOfWeekByMonth(date),
     weekCount: getCountWeek(date),

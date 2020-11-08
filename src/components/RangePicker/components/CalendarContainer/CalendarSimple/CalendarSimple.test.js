937d92c37369bd705af31b572aebbdce026d115e
asosunoff <al.sosunoff@gmail.com>
2020-11-08 14:40:42 +0300

    CalendarSimple test update

--- src/components/RangePicker/components/CalendarContainer/CalendarSimple/CalendarSimple.test.js
+++ src/components/RangePicker/components/CalendarContainer/CalendarSimple/CalendarSimple.test.js
@@ -1,40 +1,36 @@
 import React from "react";
 import { mount } from "enzyme";
+import CalendarSimple from "./CalendarSimple";
 
-import RangePicker from "../../RangePicker";
+jest.mock("../CalendarDefault", () => ({ children }) => <div>{children}</div>);
 
 describe("CalendarSimple", () => {
   let wrapper;
 
   const getByDataId = (wrapper, dataId) =>
     wrapper.find(`[data-test-id="${dataId}"]`);
-
-  const CalendarSimple = () => wrapper.find("CalendarSimple");
-  const Title = () => getByDataId(CalendarSimple(), "calendar-title");
+  const Title = () => getByDataId(wrapper, "calendar-title");
 
   const props = {
-    startDate: new Date(2020, 0, 5),
-    endDate: new Date(2020, 0, 6),
-    onRangeSelected: jest.fn(),
-    onClose: jest.fn(),
+    date: new Date(2020, 0),
+    locales: "ru",
   };
 
   beforeEach(() => {
-    wrapper = mount(<RangePicker {...props} />);
+    wrapper = mount(<CalendarSimple {...props} />);
   });
 
   it("should render", () => {
-    expect(CalendarSimple()).toHaveLength(1);
+    expect(wrapper).toHaveLength(1);
   });
 
   it("should contain title (default)", () => {
-    let localeString = new Date(
-      props.startDate.getFullYear(),
-      props.startDate.getMonth() + 1
-    ).toLocaleString(CalendarSimple().prop("locales"), {
-      month: "long",
-      year: "numeric",
-    });
+    let localeString = wrapper
+      .prop("date")
+      .toLocaleString(wrapper.prop("locales"), {
+        month: "long",
+        year: "numeric",
+      });
 
     expect(Title().find("time").text()).toBe(localeString);
   });
@@ -42,13 +38,12 @@ describe("CalendarSimple", () => {
   it("should contain title (en)", () => {
     wrapper.setProps({ locales: "en" });
 
-    const localeString = new Date(
-      props.startDate.getFullYear(),
-      props.startDate.getMonth() + 1
-    ).toLocaleString(CalendarSimple().prop("locales"), {
-      month: "long",
-      year: "numeric",
-    });
+    const localeString = wrapper
+      .prop("date")
+      .toLocaleString(wrapper.prop("locales"), {
+        month: "long",
+        year: "numeric",
+      });
 
     expect(Title().find("time").text()).toBe(localeString);
   });

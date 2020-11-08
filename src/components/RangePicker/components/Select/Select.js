c8f36172c18704b637ce83b213aa291c9bf6cd0e
asosunoff <al.sosunoff@gmail.com>
2020-11-09 02:40:54 +0300

    Select component fix

--- src/components/RangePicker/components/Select/Select.js
+++ src/components/RangePicker/components/Select/Select.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./Select.module.scss";
+import { ArrowBottom } from "../Arrow/";
 
 const Select = ({ value, onChange, items }) => {
+  const [isVisible, setVisible] = useState(false);
+  const showHandler = useCallback(() => setVisible(true), []);
+  const hideHandler = useCallback(() => setVisible(false), []);
+
   return (
-    <select className={styles.select} value={value} onChange={onChange}>
-      {items.map((item) => (
-        <option key={item.id} value={item.value}>
-          {item.text || item.value}
-        </option>
-      ))}
-    </select>
+    <div
+      className={styles.select_container}
+      onMouseOver={showHandler}
+      onMouseOut={hideHandler}
+    >
+      <select className={styles.select} value={value} onChange={onChange}>
+        {items.map((item) => (
+          <option key={item.id} value={item.value}>
+            {item.text || item.value}
+          </option>
+        ))}
+      </select>
+
+      <ArrowBottom
+        style={{
+          top: "12px",
+          right: "0px",
+          opacity: isVisible ? 1 : 0,
+        }}
+      />
+    </div>
   );
 };
 

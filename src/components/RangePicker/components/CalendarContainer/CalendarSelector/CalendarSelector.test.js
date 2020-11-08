c3738a17e09587989121fb526e0ae58a975f3ff3
asosunoff <al.sosunoff@gmail.com>
2020-11-08 15:05:43 +0300

    CalendarSelector test fix

--- src/components/RangePicker/components/CalendarContainer/CalendarSelector/CalendarSelector.test.js
+++ src/components/RangePicker/components/CalendarContainer/CalendarSelector/CalendarSelector.test.js
@@ -24,4 +24,56 @@ describe("CalendarSelector", () => {
   it("should contain two select element", () => {
     expect(wrapper.find("select")).toHaveLength(2);
   });
+
+  it("should call changeMonthHandler after select month", () => {
+    const changeMonthHandler = jest.fn();
+
+    const value = 1;
+
+    wrapper.setProps({ changeMonthHandler });
+
+    wrapper
+      .find("select")
+      .at(0)
+      .simulate("change", {
+        target: {
+          options: {
+            0: { value },
+            selectedIndex: 0,
+          },
+        },
+      });
+
+    expect(changeMonthHandler).toHaveBeenCalled();
+
+    const [[result]] = changeMonthHandler.mock.calls;
+
+    expect(result).toBe(value);
+  });
+
+  it("should call changeYearHandler after select year", () => {
+    const changeYearHandler = jest.fn();
+
+    const value = 1;
+
+    wrapper.setProps({ changeYearHandler });
+
+    wrapper
+      .find("select")
+      .at(1)
+      .simulate("change", {
+        target: {
+          options: {
+            0: { value },
+            selectedIndex: 0,
+          },
+        },
+      });
+
+    expect(changeYearHandler).toHaveBeenCalled();
+
+    const [[result]] = changeYearHandler.mock.calls;
+
+    expect(result).toBe(value);
+  });
 });

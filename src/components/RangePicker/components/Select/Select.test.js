430af75bb7862dd16f2e4b16b90bf716d8b94f05
asosunoff <al.sosunoff@gmail.com>
2020-11-09 20:07:22 +0300

    Select test added

--- src/components/RangePicker/components/Select/Select.test.js
+++ src/components/RangePicker/components/Select/Select.test.js
@@ -1,19 +1,13 @@
-/* import React from "react";
+import React from "react";
 import { mount } from "enzyme";
-import CalendarSelector from "./CalendarSelector";
+import Select from "./Select";
 
-jest.mock("../CalendarDefault", () => ({ children }) => <div>{children}</div>);
-
-describe("CalendarSelector", () => {
+describe("Select", () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = mount(
-      <CalendarSelector
-        changeMonthHandler={jest.fn()}
-        changeYearHandler={jest.fn()}
-        date={new Date(2020, 0)}
-      />
+      <Select items={[{ id: 1, value: "test" }]} onChange={jest.fn()} />
     );
   });
 
@@ -21,60 +15,21 @@ describe("CalendarSelector", () => {
     expect(wrapper).toHaveLength(1);
   });
 
-  it("should contain two select element", () => {
-    expect(wrapper.find("select")).toHaveLength(2);
+  it("should contain ArrowBottom element", () => {
+    expect(wrapper.find("ArrowBottom")).toHaveLength(1);
   });
 
-  it("should call changeMonthHandler after select month", () => {
-    const changeMonthHandler = jest.fn();
+  it("should call onChange after select", () => {
+    const onChange = jest.fn();
 
-    const value = 1;
+    wrapper.setProps({ onChange });
 
-    wrapper.setProps({ changeMonthHandler });
+    wrapper.find("select").simulate("change");
 
-    wrapper
-      .find("select")
-      .at(0)
-      .simulate("change", {
-        target: {
-          options: {
-            0: { value },
-            selectedIndex: 0,
-          },
-        },
-      });
+    expect(onChange).toHaveBeenCalled();
 
-    expect(changeMonthHandler).toHaveBeenCalled();
+    const [[result]] = onChange.mock.calls;
 
-    const [[result]] = changeMonthHandler.mock.calls;
-
-    expect(result).toBe(value);
-  });
-
-  it("should call changeYearHandler after select year", () => {
-    const changeYearHandler = jest.fn();
-
-    const value = 1;
-
-    wrapper.setProps({ changeYearHandler });
-
-    wrapper
-      .find("select")
-      .at(1)
-      .simulate("change", {
-        target: {
-          options: {
-            0: { value },
-            selectedIndex: 0,
-          },
-        },
-      });
-
-    expect(changeYearHandler).toHaveBeenCalled();
-
-    const [[result]] = changeYearHandler.mock.calls;
-
-    expect(result).toBe(value);
+    expect(result).toBe("test");
   });
 });
- */
